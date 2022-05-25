a76b12147ec8b85a562bae3efbf75a560887fbe5
spark <fabricio.jacob@outlook.com>
2022-05-24 23:42:45 -0300

    add

--- src/main/java/com/github/fabriciolfj/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/github/fabriciolfj/edgeservice/config/SecurityConfig.java
@@ -2,9 +2,7 @@ package com.github.fabriciolfj.edgeservice.config;
 
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
@@ -13,14 +11,14 @@ import org.springframework.security.oauth2.client.web.server.ServerOAuth2Authori
 import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    public static final String ACTUATOR = "/actuator/**";
 
     @Bean
     ServerOAuth2AuthorizedClientRepository auth2AuthorizedClientRepository() {
@@ -28,18 +26,28 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http,
-                                                         ReactiveClientRegistrationRepository reactiveClientRegistrationRepository) {
-        return http
-                .authorizeExchange(ex ->
-                    ex.pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
-                            .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
-                            .anyExchange().authenticated())
-                .exceptionHandling(exceptionHandlingSpec -> exceptionHandlingSpec.authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .oauth2Login(Customizer.withDefaults())
-                .logout(logoutSpec -> logoutSpec.logoutSuccessHandler(oidcLogoutSuccessHandler(reactiveClientRegistrationRepository)))
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.
+                exceptionHandling()
+                .authenticationEntryPoint((serverWebExchange, e) ->
+                        Mono.fromRunnable(() ->
+                                serverWebExchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED)
+                        )
+                ).accessDeniedHandler((serverWebExchange, e) -> Mono.fromRunnable(() ->
+                        serverWebExchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN)
+                )).and()
+                ///.redirectToHttps().and()
                 .csrf().disable()
-                .build();
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeExchange()
+                .pathMatchers(ACTUATOR, "/swagger-ui.html", "/webjars/**",
+                        "/swagger-resources/**", "/v2/api-docs/**", "/.well-known/**", "/", "/favicon.ico", "/swagger-ui/**").permitAll()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
     }
 
     @Bean//normalmente o token csrf e enviado via header, mas para o angular vamos anviar via cookie
